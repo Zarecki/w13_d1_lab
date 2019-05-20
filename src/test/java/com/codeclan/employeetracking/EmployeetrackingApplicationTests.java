d273015ecdd0d8a2da6937456d056d3d23f1d92c
ericxcross <eric.cross@sympatico.ca>
2019-05-20 13:53:48 +0100

    files added

---
+++ src/test/java/com/codeclan/employeetracking/EmployeetrackingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codeclan.employeetracking;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmployeetrackingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
